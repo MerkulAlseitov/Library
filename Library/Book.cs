cffb6ac297cabf91f250c0464dc2dff8b6a79d40
MerkulAlseitov <merkul1999@gmail.com>
2023-02-07 22:28:09 +0600

    Добавьте файлы проекта.

---
+++ Library/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Book : ILabraryItem
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public bool CheckedOut { get; set; }
+        public Book(string title, string author, string isbn, bool checkedout) 
+        {
+            Title = title;
+            Author = author;
+            ISBN= isbn;
+            CheckedOut = checkedout;
+        }
+        
+        public void CheckOut()
+        {
+            CheckedOut = true;
+        }
+ 
+        public void CheckIn()
+        {
+            CheckedOut= false;
+        }
+
+    }
+
+}
---
+++ Library/Program.cs
@@ -0,0 +1,59 @@
+﻿using Library;
+using System.Reflection;
+
+List<Book> books = new List<Book>();
+Book LoTR = new Book("LoTR", "Tolkien", "7777", true);
+Book HarryPotter = new Book("HarryPotter", "J.K. Rowling", "6969", true);
+Book MainCamp = new Book("MainCamp", "A. Hitler", "666", true);
+books.Add( LoTR );
+books.Add( HarryPotter );
+books.Add( MainCamp );
+
+foreach (var item in books)
+{
+    Console.WriteLine(item.Title + ' ' + item.ISBN + ' ' + item.Author + ' ' + item.CheckedOut);
+    Console.WriteLine("Do You want to Check Out this book?");
+    string yes = "yes";
+    string no = "no";   
+    string answer = Console.ReadLine();
+    if (answer == yes)
+    {
+        if (item.CheckedOut == true)
+        {
+            Console.WriteLine("already Checked out");
+        }
+        else
+        {
+            item.CheckOut();
+           
+        }
+    }
+    Console.WriteLine("Do You want to Check In this book?");
+    string answer2 = Console.ReadLine();
+
+    if (answer2 == yes) 
+    {
+        if (item.CheckedOut == false)
+        {
+            Console.WriteLine("already Checked in");
+        }
+        else
+        {
+            item.CheckIn();
+        }
+    }
+}
+
+foreach (var item in books)
+{
+    Console.WriteLine(item.Title + ' ' + item.ISBN + ' ' + item.Author + ' ' + item.CheckedOut);
+}
+
+
+//while(true)
+//{
+//    Console.WriteLine(LoTR.Title+' '+LoTR.ISBN + ' ' +LoTR.Author + ' ' +LoTR.CheckedOut);
+//    Console.WriteLine(HarryPotter.Title + ' ' + HarryPotter.ISBN + ' ' + HarryPotter.Author + ' ' + HarryPotter.CheckedOut);
+//    Console.WriteLine(MainCamp.Title + ' ' +MainCamp.ISBN + ' ' +MainCamp.Author + ' ' +MainCamp.CheckedOut);
+//    Console.ReadKey();
+//}
